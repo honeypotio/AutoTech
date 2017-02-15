2a22594cdd620bf7023477ed7ea04bd0f361aa73
Kado <kadodamball@hotmail.com>
2017-02-15 16:08:55 +0100

    Two way Relationship data tests and fixed broken data

--- test/data.js
+++ test/data.js
@@ -1,17 +1,17 @@
-var assert = require('assert');
-var companies = require('../data/autotech.json');
+const assert = require('assert');
+const companies = require('../data/autotech.json');
 
 describe('#Data Purity', () => {
-  it('Check for duplicate data', function() {
-    var companyNames = companies.map(company => company.name);
-    var uniqueCompanyNames = [...new Set(companyNames)];
+  it('No duplicate data', function() {
+    let companyNames = companies.map(company => company.name);
+    let uniqueCompanyNames = [...new Set(companyNames)];
     assert.equal(companyNames.length, uniqueCompanyNames.length);
   });
 
-  it('Check for missing company data', function() {
-    var companyNames = companies.map(company => company.name);
-    var relationships = ["mentoredBy", "foundedBy", "investedBy", "acquiredBy", "partneredWith", "mentors", "founded", "investedIn", "acquired"];
-    var linkedCompanies = companies.reduce((nameList, company) => {
+  it('No missing company data', function() {
+    let companyNames = companies.map(company => company.name);
+    let relationships = ["mentoredBy", "foundedBy", "investedBy", "acquiredBy", "partneredWith", "mentors", "founded", "investedIn", "acquired"];
+    let linkedCompanies = companies.reduce((nameList, company) => {
       relationships.forEach(rel => {
         if (company[rel]) {
           company[rel].forEach(linkedCompany => {
@@ -25,5 +25,39 @@ describe('#Data Purity', () => {
     }, []);
     
     assert.equal(companyNames.length, linkedCompanies.length);
+  });
+
+  it('All relationships are two way', function() {
+    const mapping = {
+      "mentoredBy": "mentors",
+      "foundedBy": "founded",
+      "investedBy": "investedIn",
+      "acquiredBy": "acquired",
+      "partneredWith": "partneredWith",
+      "mentors": "mentoredBy",
+      "founded": "foundedBy",
+      "investedIn": "investedBy",
+      "acquired": "acquiredBy"
+    };
+
+    const companiesHash = companies.reduce((acc, curr) => {
+      acc[curr.name] = curr;
+      return acc;
+    }, {});
+
+    companies.forEach(company => {
+      let relationshipKeys = Object.keys(company).filter(key => key !== "name");
+
+      relationshipKeys.forEach(relationship => {
+        company[relationship].forEach(relatedCompany => {
+          let relatedCompanyData = companiesHash[relatedCompany];
+          let reverseRelationship = mapping[relationship];
+
+          assert.ok(relatedCompanyData, `${relatedCompany} is either missing or it's name misspelt`)
+          assert.ok(relatedCompanyData[reverseRelationship], `Relationship is broken btn ${relatedCompany} and ${company.name}`);
+          assert.ok(relatedCompanyData[reverseRelationship].includes(company.name), `Relationship is broken btn ${relatedCompany} and ${company.name}`);
+        })
+      });
+    });
   });
 });
